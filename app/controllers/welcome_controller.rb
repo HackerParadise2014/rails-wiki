0e5cfa283d02ac78a4181a0ddb8aebc90157f172
Troy Anderson <the.inventor@gmail.com>
2014-09-22 12:53:22 -0600

    prep PR for using engine

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    # @welcome_article = Article.find_by_title('welcome')
-    # if @welcome_article
-    #   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-    #   @markdown_body = markdown.render(@welcome_article.text || '')
-    # end
-    #
-    # @articles = Article.order(:slug).paginate per_page: 20, page: params[:page]
+    @welcome_article = RailsWiki::Article.find_by_title('welcome')
+    if @welcome_article
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+      @markdown_body = markdown.render(@welcome_article.text || '')
+    end
+
+    @articles = RailsWiki::Article.order(:slug).paginate per_page: 20, page: params[:page]
   end
 end
